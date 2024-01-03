61066b1d82af344ebd48aa2c5c1fe8576a915576
kapila-dhilipan <120449602+kapila-dhilipan@users.noreply.github.com>
2024-01-03 18:00:19 +0530

    Update Main.tf

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -63,7 +63,7 @@ resource "aws_instance" "web" {
   instance_type          = "t2.medium"
   key_name               = "purplehaze"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
-  user_data              = templatefile("./install_jenkins.sh", {})
+  user_data              = templatefile("./install.sh", {})
   iam_instance_profile   = aws_iam_instance_profile.example_profile.name
 
   tags = {
